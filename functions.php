d528eee309befa9c3bf89c7b6329f3def6d3a681
Ali Hamza <alihamzaofficial3536@gmail.com>
2025-08-22 00:09:23 +0500

    first commit

---
+++ functions.php
@@ -0,0 +1,152 @@
+<?php
+// CM to PX
+function cm_to_px($cm)
+{
+    return $cm * (96 / 2.54);
+}
+
+// MM to PX
+function mm_to_px($mm)
+{
+    return $mm * (96 / 25.4);
+}
+
+function compress_svg($svg)
+{
+    $xml = simplexml_load_string($svg);
+    $width  = (float)$xml['width'];
+    $height = (float)$xml['height'];
+    $ns = $xml->getNamespaces(true);
+
+    $pathData = "";
+
+    // Rectangles
+    foreach ($xml->rect as $rect) {
+        $x  = (float)($rect['x'] ?? 0);
+        $y  = (float)($rect['y'] ?? 0);
+        $w  = (float)$rect['width'];
+        $h  = (float)$rect['height'];
+        $rx = (float)($rect['rx'] ?? 0);
+        $ry = (float)($rect['ry'] ?? 0);
+
+        if ($rx > 0 || $ry > 0) {
+            $r = max($rx, $ry);
+            $pathData .= "M {$x}," . ($y + $r) . "
+              A{$r},{$r} 0 0 1 " . ($x + $r) . ",$y
+              H " . ($x + $w - $r) . "
+              A{$r},{$r} 0 0 1 " . ($x + $w) . "," . ($y + $r) . "
+              V " . ($y + $h - $r) . "
+              A{$r},{$r} 0 0 1 " . ($x + $w - $r) . "," . ($y + $h) . "
+              H " . ($x + $r) . "
+              A{$r},{$r} 0 0 1 {$x}," . ($y + $h - $r) . "
+              Z ";
+        } else {
+            $pathData .= "M{$x},{$y} H" . ($x + $w) . " V" . ($y + $h) . " H{$x} Z ";
+        }
+    }
+
+    // Circles
+    foreach ($xml->circle as $circle) {
+        $cx = (float)$circle['cx'];
+        $cy = (float)$circle['cy'];
+        $r  = (float)$circle['r'];
+        $pathData .= "M " . ($cx - $r) . ",$cy
+                      a{$r},{$r} 0 1,0 " . (2 * $r) . ",0
+                      a{$r},{$r} 0 1,0 -" . (2 * $r) . ",0 Z ";
+    }
+
+    // Ellipses
+    foreach ($xml->ellipse as $ellipse) {
+        $cx = (float)$ellipse['cx'];
+        $cy = (float)$ellipse['cy'];
+        $rx = (float)$ellipse['rx'];
+        $ry = (float)$ellipse['ry'];
+        $pathData .= "M " . ($cx - $rx) . ",$cy
+                      a{$rx},{$ry} 0 1,0 " . (2 * $rx) . ",0
+                      a{$rx},{$ry} 0 1,0 -" . (2 * $rx) . ",0 Z ";
+    }
+
+    // Polygons
+    foreach ($xml->polygon as $polygon) {
+        $points = trim((string)$polygon['points']);
+        if ($points) {
+            $pathData .= "M $points Z ";
+        }
+    }
+
+    // Polylines
+    foreach ($xml->polyline as $polyline) {
+        $points = trim((string)$polyline['points']);
+        if ($points) {
+            $pathData .= "M $points ";
+        }
+    }
+
+    // Lines
+    foreach ($xml->line as $line) {
+        $x1 = (float)$line['x1'];
+        $y1 = (float)$line['y1'];
+        $x2 = (float)$line['x2'];
+        $y2 = (float)$line['y2'];
+        $pathData .= "M{$x1},{$y1} L{$x2},{$y2} ";
+    }
+
+    // Existing paths (just append)
+    foreach ($xml->path as $path) {
+        $d = trim((string)$path['d']);
+        if ($d) $pathData .= $d . " ";
+    }
+
+    // Build final <svg>
+    $newSvg = "<svg xmlns='http://www.w3.org/2000/svg' width='{$width}' height='{$height}' viewBox='0 0 {$width} {$height}'>
+        <path d='{$pathData}' fill='#333' stroke='black' fill-rule='evenodd'/>
+    </svg>";
+
+    return $newSvg;
+}
+
+// Genere Random Name
+function getRandom($length = 5)
+{
+    $random_str = "";
+    $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $codeAlphabet .= "abcdefghijklmnopqrstuvwxyz";
+    $codeAlphabet .= "0123456789";
+    $max = strlen($codeAlphabet);
+
+    for ($i = 0; $i < $length; $i++) {
+        $random_str .= $codeAlphabet[random_int(0, $max - 1)];
+    }
+
+    return $random_str;
+}
+// Generate file name
+function generate_file_name($ext, $folder_dir = '', $return_full_path = false, $length = 5)
+{
+    $file_name = getRandom($length);
+    $file_name .= '.' . $ext;
+    if ($folder_dir === '') return $file_name;
+    $file_location = $folder_dir . $file_name;
+    if (file_exists($file_location)) {
+        return generate_file_name($ext, $folder_dir);
+    } else {
+        if ($return_full_path) return $file_location;
+        return $file_name;
+    }
+}
+
+// SVG to PNG
+function svg_to_png($svg, $output)
+{
+    $inkscape_path = '"C:\\Program Files\\Inkscape\\bin\\inkscape.exe"';
+
+    $temp = generate_file_name("svg", "", true);
+    file_put_contents($temp, $svg);
+
+
+    $cmd = "{$inkscape_path} --export-type=png --export-filename="
+        . escapeshellarg($output) . " "
+        . escapeshellarg($temp);
+    exec($cmd, $output, $result);
+    @unlink($temp);
+}
---
+++ square.php
@@ -0,0 +1,84 @@
+<?php
+require_once "functions.php";
+
+$width = cm_to_px($_GET['width']); // 30 cm
+$height = cm_to_px($_GET['height']); // 20 cm
+$type = isset($_GET['type']) ? $_GET['type'] : "cornor"; // (cornor,rounded)
+$corner_radius = $type === 'rounded' ? 30 : 0;
+
+// Holes 
+$holes = ($_GET['holes'] == "true");
+$bolts = "";
+$hole_size    = isset($_GET['hole_size']) ? mm_to_px($_GET['hole_size']) : 37; // Default 37
+$hole_margin = isset($_GET['padding']) ? $_GET['padding'] : 20;
+
+
+// If holes enabled
+if ($holes) {
+    $holes = "";
+    $r = $hole_size / 2;
+
+    $positions = [
+        [$hole_margin + $r, $hole_margin + $r],                       // top-left
+        [$width - $hole_margin - $r, $hole_margin + $r],              // top-right
+        [$hole_margin + $r, $height - $hole_margin - $r],             // bottom-left
+        [$width - $hole_margin - $r, $height - $hole_margin - $r],    // bottom-right
+    ];
+
+    // Holes
+    foreach ($positions as [$cx, $cy])
+        $holes .= "<circle cx='{$cx}' cy='{$cy}' r='{$r}' fill='none' stroke='black'/>";
+
+    // Bolts
+    foreach ($positions as [$cx, $cy]) {
+        $x = $cx - $hole_size / 2;
+        $y = $cy - $hole_size / 2;
+        $bolts .= "<image href='bolts/gold.png' x='{$x}' y='{$y}' width='{$hole_size}' height='{$hole_size}' />";
+    }
+}
+
+// Get SVG
+function get_svg($content)
+{
+    global $width, $height, $corner_radius;
+
+    $svg = <<<BODY
+        <svg xmlns="http://www.w3.org/2000/svg" width="{$width}" height="{$height}" viewBox="0 0 {$width} {$height}">
+            <!-- Plate -->
+            <rect
+            x="0"
+            y="0"
+            width="{$width}"
+            height="{$height}"
+            rx="{$corner_radius}"
+            ry="{$corner_radius}"
+            fill="none"/>
+
+            {$content}
+        </svg>
+    BODY;
+
+    return $svg;
+}
+
+// Download Cliping Mask
+function download_mask()
+{
+    global $holes;
+    $svg = get_svg($holes);
+    $svg = compress_svg($svg, "svg");
+    $filename = generate_file_name("svg");
+    file_put_contents("output/$filename", $svg);
+    return $filename;
+}
+
+// Download PNG 
+function download_png()
+{
+    global $bolts;
+    $svg = get_svg($bolts, "png");
+    $filename = generate_file_name("png");
+
+    svg_to_png($svg, "output/{$filename}");
+    return $filename;
+}
