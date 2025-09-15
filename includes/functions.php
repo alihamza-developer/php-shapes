68659eae2696357761c934f4d6bdcc0f625ac11a
Ali Hamza <alihamzaofficial3536@gmail.com>
2025-09-15 10:54:02 +0500

    fix functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,17 @@
 <?php
-require_once "config.php";
 require_once 'vendor/autoload.php';
+
+define("PDF_OUTLINE_GAP", cm_to_px(0.4));
+$PDF_OUTLINE_GAP = cm_to_px(0.4);
+$PDF_OUTLINE_COLOR = "blue";
+$PDF_OUTLINE_WIDTH = "0.33578";
+$STROKE_WIDTH = 0.33578;
+$STROKE_COLOR = "#2B2A29";
+
+define("OUTPUT_PATH", "output/");
+define("SPACERS_PATH", "./spacers");
+
+@mkdir(OUTPUT_PATH);
 
 define('INKSCAPE_PATH', '"C:\\Program Files\\Inkscape\\bin\\inkscape.exe"');
 
