347d887375f74669f37971dd0e77bb5465170f5c
Ali Hamza <alihamzaofficial3536@gmail.com>
2025-09-16 17:48:33 +0500

    vintage version 1

--- vintage.php
+++ vintage.php
@@ -5,28 +5,48 @@ require_once "includes/functions.php";
 $width = cm_to_px($_GET['width']); // cm
 $height = cm_to_px($_GET['height']); // cm
 
-$start_x = get_per($width, 12);
+$half_x = $width / 2;
+$half_y = $height / 2;
+
+$start_x = get_per($width, 6.8);
 $start_y = get_per($height, 10);
-$per_25 = get_per($width, 25);
 
 
-$path = <<<PATH
-    M{$start_x},{$start_y}
-    L{$per_25},{$start_y}
+$per_x = get_per($width, 20); # X
+$per_y = get_per($width, 16.7); # Y
 
 
-PATH;
+$path = "M{$start_x},{$start_y}"; # Start Pos
+
+# Top Portion
+$path .= "H{$per_x}"; # Line Top Left
+$path .= "Q {$half_x},-{$start_y} " . $width - $per_x . ",{$start_y}"; # Top Curve
+$path .= "H" . ($width - $start_y); # Line Top Right
 
 
+// Right Portion
+$path .= "V {$per_y}"; # Line
+$path .= "Q " . ($width + $start_x) . ",{$half_y} " . ($width - $start_x) . "," . $height - $per_y; # Curve
+$path .= "V " . $height - $start_y; # Line
 
 
+// Bottom Position
+$path .= "H" . $width - $per_x; # Line
+$path .= "Q {$half_x}," . ($start_y + $height) . " " .  $per_x . "," . $height - $start_y; # Top Curve
+$path .= "H{$start_x}"; # Line
 
 
+// Left Portion
+$path .= "V" . $height - $per_y; # Line
+$path .= "Q -{$start_x},{$half_y} {$start_x}, {$per_y}"; # Curve
+$path .= "z"; # Line
 
+
+// SVG 
 $svg = <<<SVG
         <svg width="{$width}" height="{$height}" viewBox="0 0 {$width} {$height}">
             <g class="grid"></g>
-            <path d="{$path}" stroke="red" stroke-width="2" />
+            <path d="{$path}" fill="none" stroke="red" stroke-width="2" />
         </svg>
 SVG;
 
@@ -42,36 +62,37 @@ SVG;
     <title>Document</title>
 
     <style>
-        body {
+        /* body {
             display: flex;
             justify-content: center;
             align-items: center;
             width: 100vw;
             height: 100vh;
-        }
+        } */
 
         .container {
             width: <?= $width ?>px;
             height: <?= $height ?>px;
             background: #eee;
             border: 1px solid #ddd;
+            position: relative;
+
         }
 
-        .container svg {
+        .container svg:first-child {
             border: 1px solid blue;
+        }
+
+        .container .target-shape {
+            position: absolute;
+            top: 0;
+            left: 0px;
         }
     </style>
 </head>
 
 <body>
-    <div class="container">
-
-        <?= $svg ?>
-
-        <svg width="<?= $width ?>" height="<?= $height ?>" viewBox="0 0 <?php $width ?> <?php $height ?>">
-            <path d="M264 793l-132 0c-11,0 -23,-4 -31,-13 -8,-8 -13,-19 -13,-31l0 -88c-117,-123 -117,-317 0,-440l0 -89c0,-11 5,-23 13,-31 8,-8 20,-13 31,-13l132 0c252,-117 542,-117 794,0l132 0c11,0 23,5 31,13 8,8 13,20 13,31l0 89c117,123 117,317 0,440l0 88c0,12 -5,23 -13,31 -8,9 -20,13 -31,13l-132 0c-252,118 -542,118 -794,0z" stroke="#333" stroke-width="1"></path>
-        </svg>
-    </div>
+    <?= $svg ?>
 
 </body>
 
