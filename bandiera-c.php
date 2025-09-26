54c25b3f1eaeeefa1515e9b90d9f9d7b89d19ff8
Ali Hamza <alihamzaofficial3536@gmail.com>
2025-09-27 00:55:32 +0500

    bandiera c shape completed

--- bandiera-c.php
+++ bandiera-c.php
@@ -3,20 +3,21 @@ require_once "includes/functions.php";
 // For Plate
 $width = cm_to_px($_GET['width']); // cm
 $height = cm_to_px($_GET['height']); // cm
-$frame = $_GET['frame'] ?? false;
+$frame = (bool) ($_GET['frame'] ?? null);
 $padding = $FRAME_HOLES_GAP;
-
 // For Holes
 $count = intval($_GET['holes'] ?? 0); // (1,2,4,6)
 $size = mm_to_px($_GET['size'] ?? 8); // mm
 $spacer = $_GET['spacer'] ?? false; // Spacer
+$position = $_GET['position'] ?? "";
 $direction = $_GET['direction'] ?? "";
 
 
 # Path Info (don't moidfy)
-$BASE_PATH = "M269 40c-86,-27 -176,-41 -266,-40 -2,0 -3,1 -3,3 -1,90 13,180 40,266l0 59c-27,86 -41,176 -40,266 0,1 1,3 3,3 90,0 180,-13 266,-40l59 0c86,27 176,40 266,40 1,0 3,-2 3,-3 0,-90 -13,-180 -40,-266l0 -59c27,-86 40,-176 40,-266 0,-2 -2,-3 -3,-3 -90,-1 -180,13 -266,40l-59 0z";
-$PATH_WIDTH = 597;
-$PATH_HEIGHT = 597;
+$BASE_PATH = "M0 87l0 770c0,7 3,13 9,18 5,4 12,5 18,4 181,-41 366,-62 551,-63 52,0 104,3 155,11 187,39 377,57 568,53 12,0 21,-10 21,-22l0 -772c0,-6 -2,-11 -6,-15 -5,-5 -10,-7 -16,-7 -191,4 -382,-14 -569,-53 -51,-7 -103,-11 -155,-11 -188,1 -376,23 -559,65 -10,3 -17,12 -17,22z";
+$PATH_WIDTH = 1322;
+$PATH_HEIGHT = 881;
+
 
 
 // Get SVG
@@ -52,33 +53,31 @@ function get_svg($holes = "")
 // Generate Holes
 function generate($spacer = null, $gap = 0)
 {
-    global $width, $height, $count, $direction, $frame, $FRAME_GAP, $FRAME_WIDTH;
-
+    global $width, $height, $count, $position, $frame, $FRAME_GAP, $FRAME_WIDTH;
     $gap = $frame ? $FRAME_WIDTH + $FRAME_GAP : 0;
+
     $path = get_resized_path($width, $height);
     preg_match_all('/-?\d+\.?\d*/', $path, $matches);
     $cords = $matches[0];
+    $y_cord = abs($cords[1]);
 
-    $x_cord = abs($cords[66]);
-    $y_cord = abs($cords[5]);
+    $positions = ['tl', 'tr', 'bl', 'br'];
 
-    $positions = [
-        'tl',
-        'tr',
-        'bl',
-        'br'
-    ];
-
-    if ($count === 2)
-        $positions = ($direction == 'vertical') ? ['tc', 'bc'] : ['lc', 'rc'];
+    switch ((int)$count) {
+        case 2:
+            $positions = ($position === 'bottom')
+                ? ['bl', 'br']
+                : ['tl', 'tr'];
+            break;
+    }
 
     $out = '';
     foreach ($positions as $pos) {
+
         $out .= get_hole([
             'spacer' => $spacer,
             'pos' => $pos,
             'y_cord' => $y_cord,
-            'x_cord' => $x_cord,
             'pdf_gap' => $gap
         ]);
     }
@@ -93,38 +92,15 @@ function get_hole($data)
 
     $spacer = $data['spacer'];
     $pos = $data['pos'];
-    $x_cord = $data['x_cord'];
     $y_cord = $data['y_cord'];
     $pdf_gap = $data['pdf_gap'];
-
-    $h_w = $width  / 2;
-    $h_h = $height / 2;
     $r = $size / 2;
-    $gap_x = $padding + $r + $pdf_gap;
-    $gap_y = $padding + $r + $pdf_gap;
+
+    $gap = $y_cord + $padding + $r;
+
     [$vert, $horiz] = str_split($pos);
-
-    $x = ($horiz === "l") ? $gap_x : $width - $gap_x;
-    $y = ($vert === "t") ? $gap_y : $height - $gap_y;
-
-    if ($horiz === 'c') {
-        // top/bottom center
-        if ($vert === 't') {
-            $x = $h_w;
-            $y = $padding + $r + $y_cord;
-        } elseif ($vert === 'b') {
-            $x = $h_w;
-            $y = $height - ($padding + $r + $y_cord);
-        }
-        // left/right center
-        elseif ($vert === 'l') {
-            $x = $padding + $r + $pdf_gap + $x_cord;
-            $y = $h_h;
-        } elseif ($vert === 'r') {
-            $x = $width - ($padding + $r + $pdf_gap + $x_cord);
-            $y = $h_h;
-        }
-    }
+    $x = ($horiz === "l") ? ($padding + $r + $pdf_gap) : ($width - ($padding + $r + $pdf_gap));
+    $y = ($vert === "t") ? $gap + $pdf_gap : ($height - ($y_cord + $r + $pdf_gap));
 
     return make_hole([
         'x' => $x,
@@ -137,5 +113,4 @@ function get_hole($data)
 start_downloader([
     'dir' => __DIR__,
     "compress" => false
-
 ]);
